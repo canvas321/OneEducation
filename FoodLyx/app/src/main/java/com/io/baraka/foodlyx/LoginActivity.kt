585c0ab74307c3becf1d8c05f0515f7c2d854101
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-07 17:50:08 +0300

    Update Login Activity
    Fixed unresolved resource reference
    Completed UI
    Added  redirect functionality

--- FoodLyx/app/src/main/java/com/io/baraka/foodlyx/LoginActivity.kt
+++ FoodLyx/app/src/main/java/com/io/baraka/foodlyx/LoginActivity.kt
@@ -1,11 +1,42 @@
 package com.io.baraka.foodlyx
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        login()
+        redirect()
+        forgotPassword()
     }
+
+    private fun login(){
+        val login: Button = findViewById(R.id.login);
+
+        login.setOnClickListener{
+            startActivity(Intent(this, Dashboard::class.java))
+            finish()
+        }
+    }
+
+    private fun redirect(){
+        val redirectBTN: TextView = findViewById(R.id.redirect_to_signup)
+        redirectBTN.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+    }
+
+    private fun forgotPassword( ){
+        val redirectBTN: TextView = findViewById(R.id.forgot_pwd)
+        redirectBTN.setOnClickListener{
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
+        }
+
+    }
+
 }
